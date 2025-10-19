3c4ae500120024d5425256bf3018f3d97449cf84
Sérgio <francisco288.sergio@gmail.com>
2025-10-19 00:03:01 -0300

    style: troca cor do fundo para roxo mais escuro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
    
-    <div className="bg-violet-eggplant-400 h-screen" >
+    <div className="bg-violet-eggplant-500 h-screen" >
      <h1>Teste</h1>  
     </div>
     );
