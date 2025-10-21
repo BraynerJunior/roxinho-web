6c5fa29cad175ee4ac44221d43806aedf30b6732
Sérgio <francisco288.sergio@gmail.com>
2025-10-21 20:45:53 -0300

    style: edita botão(de frente com...)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import { IoPersonOutline } from "react-icons/io5";
 
 export default function Home() {
   return (
-    <div className="bg-violet-eggplant-300 min-h-[100svh] w-[100svw] justify-center flex">
+    <div className="bg-violet-eggplant-300 min-h-[100svh] w-[100svw] justify-center flex ">
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 justify-items-center w-[90%] my-10 gap-y-3 gap-x-4">
         <div className="h-full flex flex-col items-center justify-between space-y-5 w-full">
           <Card className="relative h-1/4 w-full md:mt-15 min-h-40 duration-300 ease-out shadow-2xl hover:shadow-none hover:translate-y-3">
@@ -20,7 +20,7 @@ export default function Home() {
               <CardDescription className="text-xl">Quem somos</CardDescription>
             </CardHeader>
             <div className="absolute -top-12 left-1/2 -translate-x-1/2 flex items-center justify-center w-25 h-25 rounded-full bg-purple-950">
-              <Image src="/images/mascote.png" alt="Mascote" fill className="object-cover rounded-full border-[#8232A4] border-3" /> 
+              <Image src="/images/mascote.png" alt="Mascote" fill className="object-cover rounded-full border-[#8232A4] border-3" />
             </div>
           </Card>
           <Card className="flex-grow w-full justify-start bg-violet-eggplant-600 hover:bg-violet-eggplant-500 duration-300 ease-out shadow-2xl hover:shadow-none hover:translate-y-3 text-white items-center lg:items-start ">
@@ -68,18 +68,22 @@ export default function Home() {
         </div>
 
         <div className="h-full flex flex-col space-y-5 w-full items-center justify-between">
-          <Card className="relative h-1/4 w-full md:mt-15 min-h-40  duration-300 ease-out shadow-2xl hover:shadow-none hover:translate-y-3 ">
+          <Card className="relative h-1/4 w-full md:mt-15 min-h-40  duration-300 ease-out shadow-2xl hover:shadow-none hover:translate-y-3 bg-blue-700 text-white">
             <CardHeader>
-              <CardTitle>Jogos</CardTitle>
-              <CardDescription>Caça ao tesouro</CardDescription>
+              <CardTitle className="text-3xl">Jogos</CardTitle>
+              <CardDescription className="text-white text-2xl">Caça ao tesouro</CardDescription>
             </CardHeader>
           </Card>
 
-          <Card className="flex-grow w-full justify-start lg:justify-center bg-violet-eggplant-600 hover:bg-violet-eggplant-500 duration-300 ease-out shadow-2xl hover:shadow-none hover:translate-y-3  text-white items-center lg:items-start ">
-            <CardContent className=" mt-20 pl-11 lg:pl-0">
-              <IoPersonOutline className="w-[12rem] h-full p-0 m-0" />
+          <Card className="group relative flex-grow w-full justify-end bg-violet-eggplant-600 hover:bg-violet-eggplant-500 duration-300 ease-out shadow-2xl hover:shadow-none hover:translate-y-3 text-white items-center lg:items-start overflow-hidden min-h-80 ">
+          
+            <div className="absolute inset-0 opacity-0 lg:opacity-0 group-hover:opacity-100 transition-opacity duration-500 brightness-70 delay-200">
+            <Image src="/images/pessoasAleatorio.png" alt="Imagem de uma pessoa aleatoria" width={400} height={100} className="w-full h-full object-cover object-center -z-10 " />
+            </div>
+            <CardContent className=" mt-20 pl-11 lg:pl-0 self-center transition-transform duration-700 ease-in-out group-hover:rotate-x-50 group-hover:rotate-z-45 group-hover:scale-0">
+              <IoPersonOutline className="w-[12rem] h-full p-0 m-0 " />
             </CardContent>
-            <CardHeader className="min-w-50 pl-11 lg:pl-5 lg:translate-x-0 text-center md:text-start">
+            <CardHeader className="min-w-70 pl-9 lg:pl-8 lg:translate-x-0 text-center md:text-start z-10 mt-20 ">
               <CardTitle className=" text-2xl ">De frente com...</CardTitle>
               <CardDescription className=" text-white text-xl">Saiba quem é a celebridade da vez</CardDescription>
             </CardHeader>
