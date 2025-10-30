16a636be4ee5df242c20024815a84ad13c41452d
Sérgio <francisco288.sergio@gmail.com>
2025-10-29 22:08:48 -0300

    fix: remove import não usado em (campanha news)

--- src/components/CardCampanhasNews/index.tsx
+++ src/components/CardCampanhasNews/index.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Lottie from "lottie-react";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import { Card, CardContent, CardDescription, CardTitle } from "../ui/card";
 import campanhasAnimations from "@/animations/campanhas.json"
 export default function CardCampanhaNews() {
     return (
