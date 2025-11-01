4830bc6ae8460cd2c98dd81b2ca18b1cf7f82cd0
Walther Oliveira <waltherpires@hotmail.com>
2025-10-31 22:40:12 -0300

    fix(sidebar): centraliza barra lateral

--- src/app/(private)/defrentecom/gerente/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function DeFrenteComPage() {
-    return(
-        <div className="w-screen h-screen bg-green-500">
-            <main>
-            <h1>De frente Com</h1>
-            </main>
-        </div>
-    )
-}
